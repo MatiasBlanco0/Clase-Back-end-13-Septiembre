0327449b58a1d1c40fd2b27fdb7d81bfc2263811
MatiasBlanco0 <101400282+MatiasBlanco0@users.noreply.github.com>
2022-09-15 15:52:12 -0300

    Clase del 15 de septiembre

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log();
+const express = require('express');
+const app = express();
+const port = 9000;
+
+const users = [
+    { id: 1, name: "Jhon Doe" },
+    { id: 2, name: "Jane Doe" },
+    { id: 3, name: "Jhon Smith" },
+    { id: 4, name: "Jane Smith" }
+]
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.sned("Request GET en /");
+});
+
+app.get("/users", (req, res) => {
+    res.json(users);
+});
+
+app.get("/users/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const user = users.find(user => user.id === id);
+
+    if (!user) {
+        res.status(404).send("User nor found");
+    } else {
+        res.status(200).json(user);
+    }
+});
+
+app.listen(port, () => console.log("Server emepezo en http://localhost:" + port));
